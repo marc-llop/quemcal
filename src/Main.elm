cddb655421c886c6267d5dd5f571c1394933ac5e
Marc Llop <102867909+marc-llop@users.noreply.github.com>
2024-02-13 20:02:04 +0100

    Prototype look and feel

--- src/Main.elm
+++ src/Main.elm
@@ -1,10 +1,11 @@
 module Main exposing (main)
 
 import Browser
-import Element exposing (Element, paragraph, rgb255, text)
+import Element exposing (Element, fill, height, px, rgb255, spacing, text, width)
 import Element.Background as Background
 import Element.Font as Font
 import Html exposing (Html)
+import Icons
 import Msg exposing (Msg(..))
 
 
@@ -25,13 +26,112 @@ update msg model =
 
 view : Int -> Html Msg
 view model =
+    let
+        backgroundColor state =
+            case state of
+                Pending ->
+                    colors.lightGrey
+
+                Completed ->
+                    colors.grey
+
+        listColumn state =
+            Element.column
+                [ width fill
+                , Background.color <| backgroundColor state
+                , spacing 1
+                ]
+    in
     Element.layout [ Background.color colors.black ]
-        (paragraph [ Font.color colors.lime ] [ text "Hello world!" ])
+        (Element.column
+            [ height fill, width fill, spacing 1 ]
+            [ listColumn Pending
+                [ itemView { state = Pending, text = "Potatoes" }
+                , itemView { state = Pending, text = "Cucumbers" }
+                ]
+            , listColumn Completed
+                [ itemView { state = Completed, text = "Cockles" }
+                , itemView { state = Completed, text = "Tuna" }
+                ]
+            ]
+        )
+
+
+type ItemState
+    = Completed
+    | Pending
+
+
+type alias Item =
+    { state : ItemState, text : String }
+
+
+itemText item =
+    let
+        color =
+            case item.state of
+                Completed ->
+                    [ Font.color colors.lime ]
+
+                Pending ->
+                    [ Font.color colors.lightLime, Font.glow colors.lime 2.0 ]
+    in
+    Element.el
+        (width fill :: color)
+        (text item.text)
+
+
+itemCheckbox item =
+    let
+        color =
+            case item.state of
+                Completed ->
+                    colors.lime
+
+                Pending ->
+                    colors.lightLime
+
+        icon =
+            case item.state of
+                Completed ->
+                    Icons.checkSquare
+
+                Pending ->
+                    Icons.square
+    in
+    Element.el
+        [ width (px 32)
+        , Font.color color
+        , Font.glow colors.lime 2.0
+        ]
+        (Element.html icon)
+
+
+itemView item =
+    let
+        backgroundColor =
+            case item.state of
+                Completed ->
+                    colors.black
+
+                Pending ->
+                    colors.grey
+    in
+    Element.row
+        [ width fill
+        , Element.padding 20
+        , Background.color backgroundColor
+        , Element.spacing 20
+        ]
+        [ itemCheckbox item
+        , Element.el [] <| itemText item
+        ]
 
 
 colors =
     { black = rgb255 6 2 6
-    , grey = rgb255 10 2 10
+    , grey = rgb255 24 17 24
+    , lightGrey = rgb255 48 34 48
     , lime = rgb255 159 234 34
     , lightLime = rgb255 214 246 162
     }
